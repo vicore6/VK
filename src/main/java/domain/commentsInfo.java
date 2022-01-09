18bc4fc7c50a60d0d8a3f7f76ec3abb94d43119a
vicore6 <vicore6@gmail.com>
2022-01-09 16:09:06 +0300

    OK

---
+++ src/main/java/domain/commentsInfo.java
@@ -0,0 +1,8 @@
+package ru.netology;
+
+public class CommentsInfo {
+    private int count;
+    private boolean canPost;
+  ...
+    // + get/set на все поля
+}
---
+++ src/main/java/domain/post.java
@@ -0,0 +1,10 @@
+package ru.netology;
+
+public class post {
+    private int id;
+    private int ownerId; // поле из документации
+  ...
+    private CommentsInfo commentsInfo; // в документации он типа object, под такой тип объекта мы создали отдельный класс CommentsInfo
+  ...
+// + get/set на все поля
+}
